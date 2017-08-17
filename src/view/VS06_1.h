f40065f321b300d971e4f3bdd559b97cd10d6672
Cater Tsai <caterokusai@gmail.com>
2017-08-17 23:29:25 +0800

    /modify square move

--- src/view/VS06_1.h
+++ src/view/VS06_1.h
@@ -4,17 +4,76 @@
 
 class VS06_1 : public baseView
 {
+private:
+	class squarePos
+	{
+	public:
+		void update(float delta, ofRectangle& range)
+		{
+			_pos += _vec * delta;
+			if (_pos.x <= range.getMinX())
+			{
+				_pos.x = range.getMinX();
+				_vec.x = -_vec.x;
+			}
+			else if (_pos.x > range.getMaxX())
+			{
+				_pos.x = range.getMaxX();
+				_vec.x = -_vec.x;
+			}
+
+			if (_pos.y <= range.getMinY())
+			{
+				_pos.y = range.getMinY();
+				_vec.y = -_vec.y;
+			}
+			else if (_pos.y >= range.getMaxY())
+			{
+				_pos.y = range.getMaxY();
+				_vec.y = -_vec.y;
+			}
+		}
+	
+	public:
+		ofVec2f _pos, _vec;
+	};
 public:
 	VS06_1(string path)
 		:baseView(path)
 	{
 		_canvas.allocate(cViewCanvasWidth, cViewCanvasHeight, GL_RGB);
 		_display.allocate(cViewCanvasWidth, cViewCanvasHeight, ofImageType::OF_IMAGE_COLOR);
+
+		_squareMoveRange.x = _squareCropRect[0].width * 0.5;
+		_squareMoveRange.y = _squareCropRect[0].height * 0.5;
+		_squareMoveRange.width = 500;
+		_squareMoveRange.height = 600;
+		for (int i = 0; i < cSquareNum; i++)
+		{
+			_squarePosList[i]._pos = _squareCropRect[i].getCenter();
+			_squarePosList[i]._vec.x = ofRandom(0.01, 0.1) * 600 * (rand() % 2 == 0 ? 1: -1);
+			_squarePosList[i]._vec.y = ofRandom(0.01, 0.1) * 600 * (rand() % 2 == 0 ? 1 : -1);
+		}
+		_needMove = false;
 	}
 
 	//-------------------------------
 	void update(float delta) override
 	{
+		if (_needMove)
+		{
+			for (int i = 0; i < cSquareNum; i++)
+			{
+				_squarePosList[i].update(delta, _squareMoveRange);
+				_squareCropRect[i].setFromCenter(
+					_squarePosList[i]._pos
+					, _squareCropRect[i].width
+					, _squareCropRect[i].height
+				);
+				squareMgr::GetInstance()->setUnitRect(i, _squareCropRect[i]);
+			}
+		}
+		
 		_dms.update(delta);
 		
 		_canvas.begin();
@@ -23,7 +82,6 @@ public:
 		{	
 			_dms.draw(0, 0, _canvas.getWidth(), _canvas.getHeight());
 		}
-		
 		postFilter::GetInstance()->_canvasPost.end();
 		_canvas.end();
 
@@ -60,10 +118,12 @@ public:
 		{
 		case eCtrl_ViewTrigger1:
 		{
+			_needMove = true;
 			break;
 		}
 		case eCtrl_ViewTrigger2:
 		{
+			_needMove = false;
 			break;
 		}
 		case eCtrl_ViewTrigger3:
@@ -90,11 +150,12 @@ public:
 	}
 
 private:
-	
-private:
 	DMandelbrotSet	_dms;
 	ofFbo	_canvas;
 	ofImage	_display;
 
+	ofRectangle _squareMoveRange;
 
+	bool _needMove;
+	array<squarePos, cSquareNum> _squarePosList;
 };
